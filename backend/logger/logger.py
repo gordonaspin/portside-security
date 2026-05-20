2ad78400e74d2c405fb9406c94a0721255b94161
gordonaspin <gordon.aspin@gmail.com>
2026-05-20 14:15:26 -0400

    log filename to logger

--- backend/logger/logger.py
+++ backend/logger/logger.py
@@ -46,7 +46,7 @@ def log_event(message, level="info", camera=None, file_path=None):
     color = colors.get(level,"#fff")
 
     #print(f"[{timestamp}] {camera:<8} {message}")
-    fstr = f"{camera.name + " " if camera else ""}{message}"
+    fstr = f"{camera.name + " " if camera else ""}{message}" + f" {file_path}" if file_path else ""
     match level:
         case "info": logger.info(fstr)
         case "debug": logger.debug(fstr)
